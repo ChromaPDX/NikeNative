cc35141ea85b58df9b1600aeead2274ea53b26ed
eric kuehne <eric@chroma.io>
2014-05-09 08:34:47 -0600

    freeze special card.

--- NKNikeField/MODEL/CardTypes.h
+++ NKNikeField/MODEL/CardTypes.h
@@ -21,6 +21,11 @@ typedef NS_ENUM(int32_t, CardCategory) {
     CardCategorySpecial
 };
 
+typedef NS_ENUM(int32_t, CardSpecialCategory) {
+    CardSpecialCategoryNull = 0,
+    CardSpecialCategoryFreeze = 1
+};
+
 typedef NS_ENUM(int32_t, CardMoveCategory) {
     CardMoveCategoryNull = 0,
     CardMoveCategoryBishop = 1,
